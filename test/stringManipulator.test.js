3fbf848da301b8bec820f02bbdd5eda3253944dc
Torres-ssf <sergio.uft@gmail.com>
2019-11-12 10:51:31 -0300

    fix(stickler): fixed some stickler issues

--- test/stringManipulator.test.js
+++ test/stringManipulator.test.js
@@ -1,19 +1,19 @@
-import stringManipulation from '../lib/stringManipulator'
+import stringManipulation from '../lib/stringManipulator';
 
-describe("String manipulation methods", () => {
-    test("capitalize string", () => {
-        expect(stringManipulation.capitalize("hello there")).toBe("Hello there");
+describe('String manipulation methods', () => {
+    test('capitalize string', () => {
+        expect(stringManipulation.capitalize('hello there')).toBe('Hello there');
     });
-    test("capitalize string", () => {
-        expect(stringManipulation.capitalize("you only live once")).toBe("You only live once");
+    test('capitalize string', () => {
+        expect(stringManipulation.capitalize('you only live once')).toBe('You only live once');
     });
-    test("capitalize string", () => {
-        expect(stringManipulation.capitalize("...to continue")).toBe("...to continue");
+    test('capitalize string', () => {
+        expect(stringManipulation.capitalize('...to continue')).toBe('...to continue');
     });
-    test("reverse a given string", () => {
-        expect(stringManipulation.reverse("hello")).toBe("olleh")
+    test('reverse a given string', () => {
+        expect(stringManipulation.reverse('hello')).toBe('olleh');
     });
-    test("reverse a given string", () => {
-        expect(stringManipulation.reverse("WHOAA!! What a nice job!")).toBe("!boj ecin a tahW !!AAOHW")
+    test('reverse a given string', () => {
+        expect(stringManipulation.reverse('WHOAA!! What a nice job!')).toBe('!boj ecin a tahW !!AAOHW');
     });
 })
